2f1acca56637d662001cbb25e2ff0b7c3ce20de7
jiangwei <jiangwei07061625@163.com>
2019-09-09 15:21:29 +0800

    服务通信

--- sy_api/application/controllers/Index.php
+++ sy_api/application/controllers/Index.php
@@ -8,7 +8,11 @@ class IndexController extends CommonController
 
     public function indexAction()
     {
-        $res = \SyModule\SyModuleUser::getInstance()->sendApiReq('/Index/Index/index', $_GET);
-        $this->sendRsp($res);
+        $this->SyResult->setData([
+            'msg' => 111,
+        ]);
+        $this->sendRsp();
+//        $res = \SyModule\SyModuleUser::getInstance()->sendApiReq('/Index/Index/index', $_GET);
+//        $this->sendRsp($res);
     }
 }
