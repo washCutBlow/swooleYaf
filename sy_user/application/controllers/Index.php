761f9d49288f9274917fd440fc09deb2d8679262
jiangwei <jiangwei07061625@163.com>
2019-09-11 09:31:00 +0800

    集成apidoc

--- sy_user/application/controllers/Index.php
+++ sy_user/application/controllers/Index.php
@@ -7,7 +7,14 @@ class IndexController extends CommonController
     }
 
     /**
+     * 测试1
+     * @api {get} /Index/Index/index 测试1
+     * @apiDescription 测试1
+     * @apiGroup Index
+     * @apiParam {string} name 名称
      * @SyFilter-{"field": "name","explain": "名称","type": "string","rules": {"min": 1}}
+     * @apiUse CommonSuccess
+     * @apiUse CommonFail
      */
     public function indexAction()
     {
@@ -20,8 +27,16 @@ class IndexController extends CommonController
     }
 
     /**
+     * 测试2
+     * @api {get} /Index/Index/index2 测试2
+     * @apiDescription 测试2
+     * @apiGroup Index
+     * @apiParam {string} name 名称
+     * @apiParam {number} age 年龄
      * @SyFilter-{"field": "name","explain": "名称","type": "string","rules": {"required": 1,"min": 1}}
      * @SyFilter-{"field": "age","explain": "年龄","type": "int","rules": {"required": 1,"min": 1}}
+     * @apiUse CommonSuccess
+     * @apiUse CommonFail
      */
     public function index2Action()
     {
